0291998e08897e1f54a6236dd438a47698e8fee8
stu <stu@qq.com>
2016-11-09 12:31:14 +0800

    Login

--- News/Controllers/BlogController.cs
+++ News/Controllers/BlogController.cs
@@ -33,6 +33,14 @@ namespace News.Controllers
         public ActionResult AddArticle()
         {
             return View();
+            if (Request.Cookies["isauth"] != null && Request.Cookies["isauth"].Value == "true")
+                            {
+                                return View();
+                            }
+                        else
+             {
+                                return RedirectToAction("login", "cookiedemo");
+                            }
         }
 
         //public ActionResult ArticleSave(string subject, string body)
